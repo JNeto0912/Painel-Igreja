e79a05652d14c2f99ed97f030116df5b2233b541
Neto <jose.ferreira.neto@outlook.com>
2026-03-31 11:40:24 -0300

    Agora vai

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactCompiler: true, // Certifique-se de que esta linha está removida ou comentada
-  reactStrictMode: true, // Pode manter ou remover, é uma boa prática
+  reactStrictMode: true,
+  transpilePackages: ['geist'], // Adicione esta linha
 };
 
-module.exports = nextConfig; // Use module.exports para JS
+module.exports = nextConfig;
